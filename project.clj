f27023686a3109060f2ea1464e087319d9d2db9d
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2015-04-16 09:52:57 +0200

    [build] bump version or rhizome, handles 'java.awt.headless' system property

--- project.clj
+++ project.clj
@@ -13,13 +13,14 @@
                  [org.slf4j/slf4j-simple "1.7.12"]
                  [org.clojars.smee/common "1.2.8-SNAPSHOT"]
                  ; web app
-                 [compojure "1.3.2"]
+                 [compojure "1.3.3"]
                  [ring/ring-jetty-adapter "1.3.2"] ;server
                  [liberator "0.12.2"] ;rest
                  [cheshire "5.4.0"] ;json
                  [ring/ring-json "0.3.1"] 
                  [prismatic/schema "0.4.0"] ;data schema description and coercion
-                 [rhizome "0.2.4"] ;dot visualization of graphs
+                 [rhizome "0.2.5"] ;dot visualization of graphs
+                 [com.stuartsierra/component "0.2.3"] ; run different parts as components
                  ]
   :profiles {:dev {:dependencies [[spyscope "0.1.5"]]}
              :routing-compiler {:main routing.routing-rest
